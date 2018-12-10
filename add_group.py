c1a35c093208362b2e7670b30a84da6ed1eb5ed2
Levsha765 <l.kusyj@uis.com>
2018-12-10 21:15:27 +0300

    Оптимизация 2 занятие

--- add_group.py
+++ add_group.py
@@ -12,6 +12,7 @@ class AddGroup(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -24,6 +25,7 @@ class AddGroup(unittest.TestCase):
 
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill form group
@@ -38,6 +40,7 @@ class AddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_groups_page(wd)
 
 
     def return_groups_page(self, wd):
@@ -51,22 +54,17 @@ class AddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd,  username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="zaq1", header="xsw2", footer="cde3"))
-        self.return_groups_page(wd)
         self.Logout(wd)
 
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_groups_page(wd)
         self.Logout(wd)
+
     
     def tearDown(self):
         self.wd.quit()
