8d84af9f0fe8ab549bb23742dc698619f11310de
Levsha765 <l.kusyj@uis.com>
2018-12-17 20:02:04 +0300

    Создан тест редактирования группы

--- fixture/group.py
+++ fixture/group.py
@@ -40,6 +40,27 @@ class GroupHelper:
         self.return_groups_page()
 
 
+    def test_edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit edition
+        wd.find_element_by_name("edit").click()
+        # enter changes
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("update").click()
+        self.return_groups_page()
+
+
     def return_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
---
+++ test/test_edit_first_group.py
@@ -0,0 +1,9 @@
+
+
+from model.group import Group
+
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.test_edit_first_group(Group(name="New_name", header="New_header", footer="New_footer"))
+    app.session.Logout()
