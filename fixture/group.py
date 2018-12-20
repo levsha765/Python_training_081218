fc9f1d84f93d0d94fe090367088e3bf3488a4787
l.kusyj@uiscom.ru <l.kusyj@uiscom.ru>
2018-12-20 12:41:15 +0300

    Возврат на домашнюю страницу

--- fixture/group.py
+++ fixture/group.py
@@ -19,7 +19,8 @@ class GroupHelper:
         self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
-        self.return_groups_page()
+        self.app.open_home_page()
+        #self.return_groups_page()
 
 
     def fill_group_form(self, group):
@@ -44,7 +45,7 @@ class GroupHelper:
         #submit deletion
         wd.find_element_by_name("delete").click()
         #self.return_groups_page()
-        self.return_home()
+        self.app.open_home_page()
 
 
     def modify_first_group(self, new_group_data):
@@ -57,7 +58,7 @@ class GroupHelper:
         # submit
         wd.find_element_by_name("update").click()
         #self.return_groups_page()
-        self.return_home()
+        self.app.open_home_page()
 
     def select_first_group(self):
         wd = self.app.wd
