ae7681805ec2897d50bb10249b34a28a022e0cba
Levsha765 <l.kusyj@uis.com>
2019-01-14 23:42:42 +0300

    Правки случайных данных в тестах добавления группы и контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,18 +1,57 @@
 # -*- coding: utf-8 -*-
 
-
+import pytest
 from model.contact import Contact
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_day():
+    day = string.digits[1:31]
+    return day
+
+def random_month():
+    month = ["January", "February","March","April","May","June","July","August","September","October","November",
+             "December"]
+    return random.choice(month)
+
+def random_year():
+    year = string.digits[1000:2019]
+    return year
 
 
-def test_add_contact(app):
+testdata = [Contact(firstname="", middlename="", lastname="",
+                               nickname="", company="", address="",
+                               home="", mobile="", work="", fax="",
+                               email="", email2="", email3="", homepage="",
+                               #bday="", bmonth="", byear="",
+                               #aday="", amonth="", ayear="",
+                               address2="", phone2="", notes="")] + [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 20),
+            lastname=random_string("lastname", 15), nickname=random_string("nickname", 10),
+            company=random_string("company", 10), address=random_string("address", 10),
+            home=random_string("home", 10), mobile=random_string("mobile", 10),
+            work=random_string("work", 10), fax=random_string("fax", 10), email=random_string("email", 10),
+            email2=random_string("email2", 10), email3=random_string("email3", 10),
+            homepage=random_string("homepage", 10), bday=random_day(), bmonth=random_month(),
+            byear=random_year(), aday=random_day(), amonth=random_month(), ayear=random_year(),
+            address2=random_string("fax", 10), phone2=random_string("fax", 10), notes=random_string("fax", 10)
+            )
+    for i in range(2)
+]
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="123", middlename="123", lastname="123",
-                               nickname="123", company="123", address="123",
-                               home="123", mobile="123", work="123", fax="123",
-                               email="123", email2="123", email3="123", homepage="123",
-                               bday="12", bmonth="March", byear="1960",
-                               aday="12", amonth="May", ayear="1961",
-                               address2="123", phone2="123", notes="123")
+    # contact = Contact(firstname="123", middlename="123", lastname="123",
+    #                            nickname="123", company="123", address="123",
+    #                            home="123", mobile="123", work="123", fax="123",
+    #                            email="123", email2="123", email3="123", homepage="123",
+    #                            bday="12", bmonth="March", byear="1960",
+    #                            aday="12", amonth="May", ayear="1961",
+    #                            address2="123", phone2="123", notes="123")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
@@ -21,7 +60,7 @@ def test_add_contact(app):
 
 
 
-
+#, ids=["firstname","lastname"]
 
 
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 
-
+import pytest
 from model.group import Group
+import random
+import string
 
-def test_add_group(app):
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 15))
+    for i in range(5)
+]
+#  [
+#     Group(name=name, header=header, footer=footer)
+#     for name in ["", random_string("name", 10)]
+#     for header in ["", random_string("header", 20)]
+#     for footer in ["", random_string("footer", 15)]
+#     ]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="zaq1", header="xsw2", footer="cde3")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
