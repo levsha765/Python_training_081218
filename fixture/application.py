8f345af2ef990aef4a3922bb0ab7635b3cf26368
Levsha765 <l.kusyj@uis.com>
2019-01-20 16:50:12 +0300

    Выполнено Задание №16

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ from fixture.contact import ContactHelper
 class Application:
 
 
-    def __init__(self, browser="Chrome"):
+    def __init__(self, browser, base_url):
         if browser == "Chrome":
             self.wd = webdriver.Chrome()
         elif browser == "Firefox":
@@ -19,6 +19,7 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
 
     def is_valid(self):
@@ -31,7 +32,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
 
     def destroy(self):
